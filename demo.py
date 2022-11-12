b44a608abd6d480b1c66927e643e899d5559542c
Ashis Das <105511844+ashis-das@users.noreply.github.com>
2022-11-13 00:12:23 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,5 @@
+# Subtraction of two numbers
+#take two user input number and subtract them
+num1=int(input("enter 1st integer no."))
+num2=int(input("enter 2nd integer no."))
+print("the Subtraction of",num1 ,"and",num2 ,"are",num1-num2)
